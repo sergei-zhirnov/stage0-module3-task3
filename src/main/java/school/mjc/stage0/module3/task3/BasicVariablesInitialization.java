5d30a8fb063fe1cc36837451e72e0f7c1cc2d362
sergei-zhirnov <s.zhirnov@protonmail.com>
2023-11-13 19:03:40 +0400

    Module 3, Task 3, Try 1

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -4,7 +4,7 @@ public class BasicVariablesInitialization {
     public static void main(String[] args) {
         int first = 1;
         int second = 10;
-        int third = 1000;
+        int third = 100;
 
         System.out.println(first);
         System.out.println(second);
